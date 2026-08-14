fd7ee83740810ba9320daf7ee6e447576bcbfd97
Isabelle Joubert <isabelleelenajoubert@gmail.com>
2026-08-14 15:24:13 +0200

    Create game functionality and add player buttons

--- scripts.js
+++ scripts.js
@@ -4,16 +4,34 @@
 // the game board should be a grid 
 // we need two players who are randomly assigned either noughts or crosses 
 
+let gameboardSection = document.querySelector(".gameboard");
+let noughtsPlayer = document.querySelector(".player-one");
+let crossesPlayer = document.querySelector(".player-two");
+let buttonSection = document.querySelector(".buttons")
+
 const gameboard = (() => {
 
     let array;
 
     const generateArray = () => {
+
         array = ['', '', '', '', '', '', '', '', ''];
+
+        for (let i = 0; i < array.length; i++) {
+
+            let playerSquare = document.createElement("button");
+            playerSquare.classList = "player-square";
+            playerSquare.dataset.number = `${i}`;
+            gameboardSection.appendChild(playerSquare);
+
+        }
+        
         return array;
+
     }
 
     return { generateArray };
+
 })();
 
 let newBoard = gameboard.generateArray();
@@ -22,16 +40,29 @@ let newBoard = gameboard.generateArray();
 
 function createPlayer(name, number, piece) {
 
-    const playerName = `Player ${Number(number)}: ${name}`;
+    const playerName = `${name}`;
 
     const noughtOrCross = piece;
+
+    if (piece === "0") {
+        noughtsPlayer.id = `${name}`;
+    }
+
+    else if (piece === "X") {
+        crossesPlayer.id = `${name}`;
+    };
+
+    noughtsPlayer.dataset.active = "true";
+    noughtsPlayer.style.backgroundColor = "red";
+    crossesPlayer.dataset.active = "false";
+    crossesPlayer.style.backgroundColor = "white";
 
     let score = 0
 
     const getScore = () => score;
     const increaseScore = () => { score++; };
 
-    return { playerName, getScore, increaseScore, noughtOrCross };
+    return { playerName, getScore, increaseScore, noughtOrCross, };
 }
 
 // current players
@@ -52,7 +83,56 @@ function game(playerOne, playerTwo) {
     createPlayer(playerOne);
     createPlayer(playerTwo);
 
-    function checkScore() {
+    const playerOneName = playerOne.playerName;
+    const playerTwoName = playerTwo.playerName;
+
+    
+     noughtsPlayer.addEventListener('click', () => {
+         if (noughtsPlayer.dataset.active === "true") {
+             noughtsPlayer.dataset.active = 'false';
+             crossesPlayer.dataset.active = "true";
+             crossesPlayer.style.backgroundColor = "red"
+             noughtsPlayer.style.backgroundColor = "white";
+         }
+         else if (noughtsPlayer.dataset.active === "false") {
+             noughtsPlayer.dataset.active = "true";
+             crossesPlayer.dataset.active = "false";
+             noughtsPlayer.style.backgroundColor = "red"
+             crossesPlayer.style.backgroundColor = "white";
+         }
+     })
+
+     crossesPlayer.addEventListener('click', () => {
+         if (crossesPlayer.dataset.active === 'false') {
+             noughtsPlayer.dataset.active = "false";
+             crossesPlayer.dataset.active = "true";
+             crossesPlayer.style.backgroundColor = "red"
+             noughtsPlayer.style.backgroundColor = "white";
+         }
+         else if (crossesPlayer.dataset.active === 'true') {
+             noughtsPlayer.dataset.active = "true";
+             crossesPlayer.dataset.active = "false";
+             noughtsPlayer.style.backgroundColor = "red"
+             crossesPlayer.style.backgroundColor = "white";
+         }
+     })
+    
+
+    //const checkWhoPlayed = () => {
+      //  buttonSection.addEventListener('click', () => {
+
+        //})
+    //}
+
+    //const play = () => {
+
+        //let playerSquare = document.querySelector("#player-square");
+
+        //playerSquare.addEventListener('click', () => )
+
+    //}
+
+    const checkScore = () => {
         if ((newBoard[0] === "X" && newBoard[1] === "X" && newBoard[2] === "X") 
             || (newBoard[3] === "X" && newBoard[4] === "X" && newBoard[5] === "X")
             || (newBoard[6] === "X" && newBoard[7] === "X" && newBoard[8] === "X")
@@ -64,14 +144,15 @@ function game(playerOne, playerTwo) {
 
                 if (playerOne.noughtOrCross === "X") {
                     playerOne.increaseScore();
-                    console.log(`Well done ${playerOne.playerName}, you win! Your score has increased by 1 and is now ${playerOne.getScore()}`);
-                };
+                    console.log(`Well done, ${playerOne.playerName}, you win! Your score has increased by 1 and is now ${playerOne.getScore()}`);
+                }
+
                 else if (playerTwo.noughtOrCross === "X") {
                     playerTwo.increaseScore();
                     console.log(`Well done ${playerTwo.playerName}, you win! Your score has increased by 1 and is now ${playerTwo.getScore()}`);
                 };
 
-            };
+            }
 
         else if ((newBoard[0] === "0" && newBoard[1] === "0" && newBoard[2] === "0") 
             || (newBoard[3] === "0" && newBoard[4] === "0" && newBoard[5] === "0")
@@ -85,7 +166,8 @@ function game(playerOne, playerTwo) {
                 if (playerOne.noughtOrCross === "0") {
                     playerOne.increaseScore();
                     console.log(`Well done ${playerOne.playerName}, you win! Your score has increased by 1 and is now ${playerOne.getScore()}`);
-                };
+                }
+
                 else if (playerTwo.noughtOrCross === "0") {
                     playerTwo.increaseScore();
                     console.log(`Well done ${playerTwo.playerName}, you win! Your score has increased by 1 and is now ${playerTwo.getScore()}`);
@@ -93,14 +175,43 @@ function game(playerOne, playerTwo) {
             
             }
 
-            for (let i = 0; i < newBoard.length; i++) {
-                        newBoard[i] = '';
-                    }
+        else {
+            console.log("No one has won yet! Keep playing.")
         }
+
+            for (let i = 0; i < newBoard.length; i++) {
+                newBoard[i] = '';
+            }
+
+        }
+
+        const resetBoard = () => {
+
+            for (let i = 0; i < newBoard.length; i++) {
+                newBoard[i] = '';
+            }
+
+            return newBoard;
+
+        }
+
+    return { playerOneName, playerTwoName, checkScore, resetBoard, }
     
 }
 
-game(izzy, dan)
+let newGame = game(izzy, dan);
+
+newBoard[2] = "0"
+newBoard[3] = "X"
+newBoard[4] = "X"
+newBoard[7] = "X"
+newBoard[8] = "X"
+newBoard[2] = "0"
+newBoard[3] = "X"
+newBoard[4] = "X"
+newBoard[7] = "X"
+newBoard[8] = "X"
+
 
 // functionality that checks player's score
 
