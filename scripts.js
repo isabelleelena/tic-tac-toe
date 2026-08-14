77834bb509feab7320f98fe07c58bc1b745948d4
Isabelle Joubert <isabelleelenajoubert@gmail.com>
2026-08-14 11:58:06 +0200

    Start to grapple with logic of game

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ const gameboard = (() => {
     let array;
 
     const generateArray = () => {
-        array = ['1, 1', '1, 2', '1, 3', '2, 1', '2, 2', '2, 3', '3, 1', '3, 2', '3, 3'];
+        array = ['', '', '', '', '', '', '', '', ''];
         return array;
     }
 
@@ -20,35 +20,107 @@ let newBoard = gameboard.generateArray();
 
 // Now I've got my board, I need to make my players. This should probably be a factory function!
 
-function createPlayer(name, number) {
-
-    const noughtOrCross = () => {
-        
-        let randomInt = Math.floor(Math.random() * 2) + 1;
-        let playerPiece
-
-        if (randomInt === 1) {
-            playerPiece = "nought";
-            return { playerPiece };
-        }
-        else if (randomInt === 2) {
-            playerPiece = "cross";
-            return { playerPiece };
-        }
-    }
+function createPlayer(name, number, piece) {
 
     const playerName = `Player ${Number(number)}: ${name}`;
+
+    const noughtOrCross = piece;
 
     let score = 0
 
     const getScore = () => score;
     const increaseScore = () => { score++; };
 
-    return {playerName, getScore, increaseScore, noughtOrCross};
+    return { playerName, getScore, increaseScore, noughtOrCross };
 }
 
-let izzy = createPlayer("izzy", "1");
-let dan = createPlayer("dan", 2)
+// current players
 
-izzy.increaseScore();
-izzy.increaseScore();
+let izzy = createPlayer("izzy", "1", "0");
+let dan = createPlayer("dan", 2, "X")
+
+// How to play the game?
+
+// First, each player needs to be able to place a piece on the board
+
+// Then, as each piece is placed, the game needs to check if there are three of the same piece in a row
+
+// If there are not, the game continues
+
+function game(playerOne, playerTwo) {
+
+    createPlayer(playerOne);
+    createPlayer(playerTwo);
+
+    function checkScore() {
+        if ((newBoard[0] === "X" && newBoard[1] === "X" && newBoard[2] === "X") 
+            || (newBoard[3] === "X" && newBoard[4] === "X" && newBoard[5] === "X")
+            || (newBoard[6] === "X" && newBoard[7] === "X" && newBoard[8] === "X")
+            || (newBoard[0] === "X" && newBoard[3] === "X" && newBoard[6] === "X")
+            || (newBoard[4] === "X" && newBoard[7] === "X" && newBoard[3] === "X")
+            || (newBoard[2] === "X" && newBoard[5] === "X" && newBoard[8] === "X")
+            || (newBoard[0] === "X" && newBoard[4] === "X" && newBoard[8] === "X")
+            || (newBoard[2] === "X" && newBoard[4] === "X" && newBoard[6] === "X")) {
+
+                if (playerOne.noughtOrCross === "X") {
+                    playerOne.increaseScore();
+                    console.log(`Well done ${playerOne.playerName}, you win! Your score has increased by 1 and is now ${playerOne.getScore()}`);
+                };
+                else if (playerTwo.noughtOrCross === "X") {
+                    playerTwo.increaseScore();
+                    console.log(`Well done ${playerTwo.playerName}, you win! Your score has increased by 1 and is now ${playerTwo.getScore()}`);
+                };
+
+            };
+
+        else if ((newBoard[0] === "0" && newBoard[1] === "0" && newBoard[2] === "0") 
+            || (newBoard[3] === "0" && newBoard[4] === "0" && newBoard[5] === "0")
+            || (newBoard[6] === "0" && newBoard[7] === "0" && newBoard[8] === "0")
+            || (newBoard[0] === "0" && newBoard[3] === "0" && newBoard[6] === "0")
+            || (newBoard[4] === "0" && newBoard[7] === "0" && newBoard[3] === "0")
+            || (newBoard[2] === "0" && newBoard[5] === "0" && newBoard[8] === "0")
+            || (newBoard[0] === "0" && newBoard[4] === "0" && newBoard[8] === "0")
+            || (newBoard[2] === "0" && newBoard[4] === "0" && newBoard[6] === "0")) {
+
+                if (playerOne.noughtOrCross === "0") {
+                    playerOne.increaseScore();
+                    console.log(`Well done ${playerOne.playerName}, you win! Your score has increased by 1 and is now ${playerOne.getScore()}`);
+                };
+                else if (playerTwo.noughtOrCross === "0") {
+                    playerTwo.increaseScore();
+                    console.log(`Well done ${playerTwo.playerName}, you win! Your score has increased by 1 and is now ${playerTwo.getScore()}`);
+                };
+            
+            }
+
+            for (let i = 0; i < newBoard.length; i++) {
+                        newBoard[i] = '';
+                    }
+        }
+    
+}
+
+game(izzy, dan)
+
+// functionality that checks player's score
+
+
+
+
+// random assignment of piece 
+
+
+    //const noughtOrCross = () => {
+//
+  //      let randomInt = Math.floor(Math.random() * 2) + 1;
+    //    let playerPiece
+//
+  //      if (randomInt === 1) {
+    //        playerPiece = "nought";
+      //      return { playerPiece };
+        //}
+//        else if (randomInt === 2) {
+  //          playerPiece = "cross";
+    //        return { playerPiece };
+      //  }
+    //}
